71316fe929b403a809101d66ef8ec0a51bdfcae1
vansh391 <sujatarathor100@gmail.com>
2025-02-05 14:16:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7978650498:AAHv_FvNxQCPp5tB3ZWFifFl_j804OOByow")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Vansh_Rathor")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6597146348")
+
+
+
+
+
+
+
+  
